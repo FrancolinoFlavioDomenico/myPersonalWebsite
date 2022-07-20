b685b71deb2a7040705260b0d4f7c0894a4697b5
Flavio Francolino <francolinoflavio@gmail.com>
2022-07-20 02:03:59 +0200

    aggiunti commenti informativi

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -5,6 +5,7 @@ const app = express(),
 
 const fs = require('fs');
 
+//setto le cors policy per accettrae richieste da qualunque sito
 const cors = require('cors');
 app.use(cors());
 
@@ -12,6 +13,10 @@ const users = [];
 
 app.use(bodyParser.json());
 
+
+/**
+ * route per il json dei dati da inserire nel cv
+ */
 app.get('/cvData', (req, res) => {
     let rawdata = fs.readFileSync('./dataFile/cvData.json');
     let student = JSON.parse(rawdata);
